1fa7ac7e4794813aa04f0986a9a3fd1df78eb42d
browles <rowlesben@gmail.com>
2017-11-12 16:44:07 -0800

    refactor to neaderthal -- still quite slow

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.443"]
                  [net.mikera/vectorz-clj "0.47.0"]
-                 [net.mikera/core.matrix "0.61.0"]])
+                 [net.mikera/core.matrix "0.61.0"]
+                 [uncomplicate/neanderthal "0.17.0"]
+                 [uncomplicate/clojurecuda "0.2.0"]])
--- src/ml/nn.clj
+++ src/ml/nn.clj
@@ -1,9 +1,13 @@
 (ns ml.nn
-  (:require [clojure.core.matrix :as m]
-            [clojure.core.matrix.stats :as mstats]
-            [ml.utils :refer :all]))
+  (:require [ml.math :refer :all]
+            [ml.utils :refer :all]
+            [uncomplicate.commons.core :refer [with-release]]
+            [uncomplicate.neanderthal.core :as ncore]
+            [uncomplicate.neanderthal.cuda :as ncuda]
+            [uncomplicate.neanderthal.native :as nnat]
+            [uncomplicate.neanderthal.vect-math :as nvm]))
 
-(m/set-current-implementation :vectorz)
+(set! *warn-on-reflection* true)
 
 (def activation-fn->f
   {:sigmoid sigmoid
@@ -25,20 +29,18 @@
   {:log-loss log-loss'
    :sq-diff sq-diff'})
 
-(defrecord Layer [W W-T b activation-fn])
+(defrecord Layer [W b activation-fn])
 (defrecord Network [n-inputs n-outputs layers loss-fn])
 (defrecord LayerActivation [input output])
 (defrecord NetworkActivation [output layer-activations])
-(defrecord LayerGradient [dW db])
-(defrecord NetworkGradient [input-dZ input-layer layer-gradients])
+(defrecord LayerGradient [dZ dW db])
+(defrecord NetworkGradient [next-dZ next-layer layer-gradients])
 
 (defn new-layer [n m activation-fn]
-  (let [W (randn n m 10)]
-    (Layer.
-      W
-      (m/transpose W)
-      (randn n 1 1)
-      activation-fn)))
+  (Layer.
+    (n-randn n m 10)
+    (n-randn n 1 1)
+    activation-fn))
 
 (defn new-network [n-inputs layer-specs loss-fn]
   (reduce (fn [network [n-neurons activation-fn]]
@@ -54,67 +56,114 @@
           layer-specs))
 
 (defn activate-layer [input layer]
-  (let [{:keys [W b activation-fn]} layer]
-    (->> (m/mmul W input)
-         (#(m/add % (broadcast-col b (m/shape %))))
-         (m/emap (get activation-fn->f activation-fn)))))
+  (let [{:keys [W b activation-fn]} layer
+        [m n] (n-shape W)
+        [_ nx-samples] (n-shape input)
+        B (n-broadcast-col b [m nx-samples])]
+    (-> (ncore/mm W input)
+        (ncore/axpy B)
+        (ncore/alter! (get activation-fn->f activation-fn)))))
+
+(defn activate-layer-gpu [input layer nx-samples]
+  (ncuda/with-default-engine
+    (let [{:keys [W b activation-fn]} layer
+          [m n] (n-shape W)]
+      (with-release [gpu-input (n-to-gpu! input)
+                     gpu-W (n-to-gpu! W)
+                     gpu-B (n-to-gpu! (n-broadcast-col b [m nx-samples]))
+                     gpu-W-input (ncore/mm gpu-W gpu-input)]
+        (-> gpu-W-input
+            (ncore/axpy! gpu-B)
+            n-from-gpu!
+            (ncore/alter! (get activation-fn->f activation-fn)))))))
+
+(defn compute-dZ-gpu [layer activation output-layer output-dZ]
+  (ncuda/with-default-engine
+    (let [{:keys [activation-fn]} layer
+          W (:W output-layer)
+          [m n] (n-shape W)
+          [zm zn] (n-shape output-dZ)
+          [am an] (n-shape activation)]
+      (with-release [gpu-W-T (n-to-gpu! (nnat/fge (ncore/trans W)))
+                     gpu-output-dZ (n-to-gpu! output-dZ)
+                     gpu-W-T-output-dZ (ncore/mm gpu-W-T gpu-output-dZ)
+                     gpu-activation-dz (n-to-gpu! (ncore/alter! (ncore/copy activation)
+                                                                (get activation-fn->dfdz activation-fn)))]
+        (-> gpu-W-T-output-dZ
+            (nvm/mul! gpu-activation-dz)
+            n-from-gpu!)))))
+
+(defn compute-output-dZ [A Y dfda dfdz]
+  ; TODO: vectorize
+  (let [[Ym Yn] (n-shape Y)]
+    (nnat/fge Ym Yn
+              (mapcat (fn [a-col y-col]
+                        (mapv
+                          (fn [a y]
+                            (* (dfda a y) (dfdz a)))
+                          a-col
+                          y-col))
+                      (ncore/cols A)
+                      (ncore/cols Y)))))
+
+(defn compute-gradient-gpu [input dZ nx-samples]
+  (ncuda/with-default-engine
+    (let [[zm _] (n-shape dZ)]
+      (with-release [gpu-input-T (n-to-gpu! (nnat/fge (ncore/trans input)))
+                     gpu-dZ (n-to-gpu! dZ)
+                     gpu-dZ-input-T (ncore/mm gpu-dZ gpu-input-T)
+                     gpu-dW (ncore/scal! (/ 1.0 nx-samples) gpu-dZ-input-T)]
+        (LayerGradient.
+          dZ
+          (n-from-gpu! gpu-dW)
+          (->> (nnat/fge zm 1 (mapv ncore/sum (ncore/rows dZ)))
+               (ncore/scal! (/ 1.0 nx-samples))))))))
 
 (defn forward-prop [network X]
-  (reduce (fn [network-activation layer]
-            (let [{:keys [output layer-activations]} network-activation
-                  activation (activate-layer output layer)]
-              (assoc network-activation
-                :output activation
-                :layer-activations (conj layer-activations
-                                         (LayerActivation. output activation)))))
-          (NetworkActivation.
-            X
-            [])
-          (:layers network)))
-
-(defn compute-gradient [dZ n-samples input]
-  (LayerGradient.
-    (m/div (m/mmul dZ (m/transpose input)) n-samples)
-    (m/div (m/matrix (mapv (comp vector mstats/sum) (m/rows dZ))) n-samples)))
+  (let [[_ nx-samples] (n-shape X)]
+    (reduce (fn [network-activation layer]
+              (let [{:keys [output layer-activations]} network-activation
+                    activation (activate-layer-gpu output layer nx-samples)]
+                (assoc network-activation
+                  :output activation
+                  :layer-activations (conj layer-activations
+                                           (LayerActivation. output activation)))))
+            (NetworkActivation.
+              X
+              [])
+            (:layers network))))
 
 (defn backward-prop [network network-activation X Y]
-  (let [[_ n-samples] (m/shape X)
+  (let [[_ nx-samples] (n-shape X)
         [output-layer & rev-layers] (reverse (:layers network))
         [output-activation & rev-activations] (reverse (:layer-activations network-activation))
-        output-dZ (m/emap (fn [a y]
-                            (* ((get loss-fn->dfda (:loss-fn network)) a y)
-                               ((get activation-fn->dfdz (:activation-fn output-layer)) a)))
-                          (:output output-activation)
-                          Y)]
+        dfda (get loss-fn->dfda (:loss-fn network))
+        dfdz (get activation-fn->dfdz (:activation-fn output-layer))
+        output-dZ (compute-output-dZ (:output output-activation) Y dfda dfdz)]
     (-> (reduce (fn [network-gradient [layer layer-activation]]
-                  (let [{:keys [input-dZ input-layer layer-gradients]} network-gradient
-                        {:keys [activation-fn]} layer
+                  (let [{:keys [next-dZ next-layer layer-gradients]} network-gradient
                         {:keys [input output]} layer-activation]
-                    (let [dZ (-> (m/mmul (:W-T input-layer) input-dZ)
-                                 (m/mul (m/emap (get activation-fn->dfdz activation-fn)
-                                                output)))
-                          layer-gradient (compute-gradient dZ n-samples input)]
+                    (let [dZ (compute-dZ-gpu layer output next-layer next-dZ)
+                          layer-gradient (compute-gradient-gpu input dZ nx-samples)]
                       (assoc network-gradient
-                        :input-dZ dZ
-                        :input-layer layer
+                        :next-dZ dZ
+                        :next-layer layer
                         :layer-gradients (conj layer-gradients layer-gradient)))))
                 (NetworkGradient.
                   output-dZ
                   output-layer
-                  [(compute-gradient output-dZ n-samples (:input output-activation))])
+                  [(compute-gradient-gpu (:input output-activation) output-dZ nx-samples)])
                 (mapv vector rev-layers rev-activations))
-        (update :layer-gradients reverse))))
+        (update :layer-gradients (comp vec reverse)))))
 
 (defn update-weights [network network-gradient learning-rate]
   (assoc network
     :layers (mapv (fn [layer layer-gradient]
                     (let [{:keys [W b]} layer
-                          {:keys [dW db]} layer-gradient
-                          new-W (m/add-scaled W dW (- learning-rate))]
+                          {:keys [dW db]} layer-gradient]
                       (assoc layer
-                        :W new-W
-                        :W-T (m/transpose new-W)
-                        :bias (m/add-scaled b db (- learning-rate)))))
+                        :W (ncore/axpy (- learning-rate) dW W)
+                        :b (ncore/axpy (- learning-rate) db b))))
                   (:layers network)
                   (:layer-gradients network-gradient))))
 
@@ -125,15 +174,15 @@
 
 (defn sgd [network learning-rate X Y]
   (reduce (fn [n [x y]]
-            (train n learning-rate (mapv vector x) (mapv vector y)))
+            (train n learning-rate x y))
           network
-          (mapv vector (m/columns X) (m/columns Y))))
+          (mapv vector (ncore/cols X) (ncore/cols Y))))
 
 (defn batched-sgd [network batch-size learning-rate X Y]
   (reduce (fn [n [X Y]]
             (train n learning-rate X Y))
           network
-          (mapv vector (submatrices X batch-size) (submatrices Y batch-size))))
+          (mapv vector (n-submatrices X batch-size) (n-submatrices Y batch-size))))
 
 (defn feed-forward [network X]
   (reduce
@@ -142,9 +191,16 @@
     (:layers network)))
 
 (defn compute-cost [network X Y]
-  (-> (m/emap (get loss-fn->f (:loss-fn network))
-              (feed-forward network X)
-              Y)
-      mstats/sum
-      (m/get-row 0)
-      mstats/mean))
+  ; TODO: vectorize
+  (let [[Ym Yn] (n-shape Y)
+        loss-fn (get loss-fn->f (:loss-fn network))]
+    (-> (nnat/fge Ym Yn
+                  (mapcat (fn [a-col y-col]
+                            (mapv
+                              loss-fn
+                              a-col
+                              y-col))
+                          (ncore/cols (feed-forward network X))
+                          (ncore/cols Y)))
+        ncore/sum
+        (/ Yn))))
